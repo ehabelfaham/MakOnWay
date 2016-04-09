e1d768a7207e8ffaf30a4fbcc9097d8644f90495
ehabelfaham <ehab.elfaham@gmail.com>
2016-04-10 00:57:44 +0200

    adding MOW.DAL

--- MOW.DAL/Properties/AssemblyInfo.cs
+++ MOW.DAL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("37cc3afd-86b2-41f6-a7d4-f8bd958705de")]
+[assembly: Guid("a9964ffe-a84b-4a8f-a815-7d7d670bf8fe")]
 
 // Version information for an assembly consists of the following four values:
 //
